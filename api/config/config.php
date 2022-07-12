13b9558af980ec72748db28ab071f1c8c98bfdef
Città di Treviso <88876912+comunetreviso@users.noreply.github.com>
2022-07-12 16:37:27 +0200

    Add files via upload

---
+++ api/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define("ROOT_PATH", realpath(dirname(__FILE__) . "/../"));
+
+// parametri database
+
+define("DB_USER", "usr_bo_inno_tv");
+define("DB_PWD", "password");
+define("DB_HOST", "localhost");
+define("DB_NAME", "bo_inno_tv");
+define("DB_PORT", "3306");
+
+// credenziali API
+
+define("API_USER", "usr_bo_inno_tv");
+define("API_PW", "Inn0TV_2022!");
---
+++ webapp/app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("MODEL_PATH", APPLICATION_PATH . DS . "models" . DS);
+define("VIEW_PATH", APPLICATION_PATH . DS . "views" . DS);
+define("LIB_PATH", APPLICATION_PATH . DS . "lib" . DS);
+
+// credenziali API
+
+define("API_URL", "http://test-innotv.api:8080/");
+define("API_USER", "usr_bo_inno_tv");
+define("API_PW", "Inn0TV_2022!");
