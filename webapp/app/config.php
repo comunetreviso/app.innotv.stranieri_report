eea8099a914f67bb35e496ab1fa355d7245df876
Città di Treviso <88876912+comunetreviso@users.noreply.github.com>
2022-07-12 17:07:39 +0200

    Update config.php

--- webapp/app/config.php
+++ webapp/app/config.php
@@ -6,6 +6,6 @@ define("LIB_PATH", APPLICATION_PATH . DS . "lib" . DS);
 
 // credenziali API
 
-define("API_URL", "http://test-innotv.api:8080/");
+define("API_URL", "https://innotv.it/apigw/api/");
 define("API_USER", "usr_bo_inno_tv");
 define("API_PW", "Inn0TV_2022!");
